30cbbb164c66e0dcb881d542a0dbd66dd8c88764
TheYounglings <maxx1716@edu.nextkbh.dk>
2025-02-19 15:27:56 +0100

    Update model.py
    Fik params til at fungere så nu hentes alle astreoider på et givent tidspunkt uden nogen problemer (:

--- model.py
+++ model.py
@@ -14,16 +14,16 @@ class UserModel:
 
         params = {
         "start_date" : currentDate,
-        "end_date	": currentDate,
+        "end_date": currentDate,
         "api_key" : self.accessId,
 
         }
         
-        self.endPoint = f'https://api.nasa.gov/neo/rest/v1/feed?start_date={currentDate}&end_date={currentDate}&api_key={self.accessId}'
+        self.endPoint = f'https://api.nasa.gov/neo/rest/v1/feed'
 
         url = self.endPoint
 
-        return requests.get(url)
+        return requests.get(url,params=params)
 
     def fetchData(self):
             response = self.request()
